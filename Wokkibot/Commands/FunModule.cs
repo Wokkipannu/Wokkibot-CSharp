2e4d0dfe8f48211e8d45467f0c0c0032f68cce1d
Wokki <iiro@kooditaikuri.fi>
2021-06-24 19:11:36 +0300

    New: Added a color command

--- Wokkibot/Commands/FunModule.cs
+++ Wokkibot/Commands/FunModule.cs
@@ -146,4 +146,67 @@ public class FunModule : SlashCommandModule
             await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, builder);
         }
     }
+
+    [SlashCommand("color", "Change your color")]
+    [RequireGuildId(827090972172091392)]
+    public async Task ColorCommand(InteractionContext ctx, [Option("color", "The color you want for your username")] string color)
+    {
+        Dictionary<string, ulong> colors = new Dictionary<string, ulong>()
+        {
+            { "darkblue", 857643057436426281 },
+            { "lightblue", 857643192782028810 },
+            { "cyan", 857643932016181268 },
+            { "green", 857643539588317215 },
+            { "orange", 857643964486909972 },
+            { "red", 834482476113330236 },
+            { "brown", 857644264070184961 },
+            { "pink", 857644506449838090 },
+            { "purple", 857643010631139389 },
+            { "yellow", 857644716593905674 }
+        };
+
+        if (colors.Keys.Contains(color.ToLower()))
+        {
+            ulong roleId;
+            colors.TryGetValue(color, out roleId);
+            DiscordRole role = ctx.Guild.GetRole(roleId);
+
+            var roles = ctx.Member.Roles;
+            foreach (var r in roles)
+            {
+                if (colors.Keys.Contains(r.Name))
+                {
+                    await ctx.Member.RevokeRoleAsync(role, "Color change request");
+                }
+            }
+            await ctx.Member.GrantRoleAsync(role);
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"You've been given role {color}"));
+        }
+        else
+        {
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("Invalid color. List of valid colors available at https://peepo.land/wokkibot/colors.html"));
+        }
+    }
+
+    public class RequireGuildIdAttribute : SlashCheckBaseAttribute
+    {
+        public ulong Id;
+
+        public RequireGuildIdAttribute(ulong id)
+            => Id = id;
+
+        public override async Task<bool> ExecuteChecksAsync(InteractionContext ctx)
+            => ctx.Guild.Id == Id;
+    }
+
+    public class RequireUserIdAttribute : SlashCheckBaseAttribute
+    {
+        public ulong Id;
+
+        public RequireUserIdAttribute(ulong id)
+            => Id = id;
+
+        public override async Task<bool> ExecuteChecksAsync(InteractionContext ctx)
+            => ctx.User.Id == Id;
+    }
 }
